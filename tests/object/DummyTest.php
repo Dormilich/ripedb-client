f1c766cced8526aca63112676a2cc489e8523722
Bertold von Dormilich <Dormilich@netscape.net>
2015-07-23 18:38:59 +0200

    adding tests about the setup of attributes

--- tests/object/DummyTest.php
+++ tests/object/DummyTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Dormilich\WebService\RIPE\AttributeInterface as A;
 use Dormilich\WebService\RIPE\Dummy;
 
 class DummyTest extends PHPUnit_Framework_TestCase
@@ -9,5 +10,36 @@ class DummyTest extends PHPUnit_Framework_TestCase
 		$obj = new Dummy('foo', 'bar');
 		$this->assertSame('foo', $obj->getType());
 		$this->assertSame('bar', $obj->getPrimaryKeyName());
+
+		$obj = new Dummy('foo');
+		$this->assertSame('foo', $obj->getType());
+		$this->assertSame('foo', $obj->getPrimaryKeyName());
+	}
+
+	public function testNonExistingAttributeGetsCreated()
+	{
+		$obj = new Dummy('foo');
+		$obj['fizz'] = 'buzz';
+
+		$this->assertInstanceOf('\Dormilich\WebService\RIPE\Attribute', 
+			$obj->getAttribute('fizz'));
+	}
+
+	public function testDefaultCreatedAttributeProperties()
+	{
+		$obj = new Dummy('foo');
+		$obj['fizz'] = 'buzz';
+
+		$this->assertFalse($obj->getAttribute('fizz')->isRequired());
+		$this->assertTrue($obj->getAttribute('fizz')->isMultiple());
+	}
+
+	public function testSetupAttributeProperties()
+	{
+		$obj = new Dummy('foo');
+		$obj->setupAttribute('buzz', A::REQUIRED, A::SINGLE);
+
+		$this->assertTrue($obj->getAttribute('buzz')->isRequired());
+		$this->assertFalse($obj->getAttribute('buzz')->isMultiple());
 	}
 }
