68468aaf7f06c24694ababf2d3c7d0136804ed01
Bertold von Dormilich <Dormilich@netscape.net>
2015-09-19 17:21:19 +0200

    added method descriptions

--- src/Dormilich/WebService/RIPE/AttributeValue.php
+++ src/Dormilich/WebService/RIPE/AttributeValue.php
@@ -8,19 +8,43 @@ use Dormilich\WebService\RIPE\Exceptions\InvalidValueException;
 
 class AttributeValue
 {
+    /**
+     * @var string
+     */
     protected $value = '';
 
+    /**
+     * @var string
+     */
     protected $comment;
 
+    /**
+     * @var string RPSL object type.
+     */
     protected $type;
 
+    /**
+     * @var string RIPE DB URL.
+     */
     protected $link;
 
+    /**
+     * Create value object from attribute value string.
+     * 
+     * @param string $value Attribute value string.
+     * @return self
+     */
     public function __construct($value)
     {
         $this->value = (string) $value;
     }
 
+    /**
+     * Return the string value of the attribute. If a comment is set, 
+     * append the comment to the value.
+     * 
+     * @return string
+     */
     public function __toString()
     {
         if (!empty($this->comment)) {
@@ -29,11 +53,22 @@ class AttributeValue
         return $this->value;
     }
 
+    /**
+     * Get the bare attribute value.
+     * 
+     * @return type
+     */
     public function getValue()
     {
         return $this->value;
     }
 
+    /**
+     * Set the comment string.
+     * 
+     * @param string $value Comment
+     * @return self
+     */
     public function setComment($value)
     {
         $this->comment = (string) $value;
@@ -41,11 +76,23 @@ class AttributeValue
         return $this;
     }
 
+    /**
+     * Get the comment string.
+     * 
+     * @return string
+     */
     public function getComment()
     {
         return $this->comment;
     }
 
+    /**
+     * Set the object type. Only makes sense if the attribute is a reference 
+     * to another RIPE DB object.
+     * 
+     * @param string $value One of the types of the RPSL objects.
+     * @return self
+     */
     public function setType($value)
     {
         $this->type = (string) $value;
@@ -53,11 +100,22 @@ class AttributeValue
         return $this;
     }
 
+    /**
+     * Get the object type.
+     * 
+     * @return string The set object type or NULL if not set.
+     */
     public function getType()
     {
         return $this->type;
     }
 
+    /**
+     * Set the RIPE DB URL of the referenced object.
+     * 
+     * @param string $value RIPE DB URL.
+     * @return self
+     */
     public function setLink($value)
     {
         $link = (string) $value;
@@ -69,18 +127,26 @@ class AttributeValue
         return $this;
     }
 
+    /**
+     * Get the reference URL.
+     * 
+     * @return string RIPE DB URL or NULL if not set.
+     */
     public function getLink()
     {
         return $this->link;
     }
 
+    /**
+     * Get an RPSL object for lookup purposes (i.e. only the primary loopup key is set).
+     * 
+     * @return Object Empty RPSL object.
+     * @throws InvalidDataTypeException Attribute is not a referenced object.
+     * @throws InvalidValueException Invalid object type.
+     */
     public function getObject()
     {
-        if (!$this->type) {
-            throw new InvalidDataTypeException('This attribute is not a referenced object.');
-        }
-
-        $class = __NAMESPACE__ . '\\RPSL\\' . $this->getClassName($this->type);
+        $class = $this->getClassName();
 
         if (!class_exists($class)) {
             throw new InvalidValueException('This object type does not exist.');
@@ -89,8 +155,19 @@ class AttributeValue
         return new $class($this->value);
     }
 
-    private function getClassName($type)
+    /**
+     * Convert object type into object class name.
+     * 
+     * @return string
+     * @throws InvalidDataTypeException Attribute is not a referenced object.
+     */
+    private function getClassName()
     {
-        return str_replace(' ', '', ucwords(str_replace('-', ' ', $this->type)) );
+        if (!$this->type) {
+            throw new InvalidDataTypeException('This attribute is not a referenced object.');
+        }
+
+        $class = str_replace(' ', '', ucwords(str_replace('-', ' ', $this->type)) );
+        return __NAMESPACE__ . '\\RPSL\\' . $class;
     }
 }
